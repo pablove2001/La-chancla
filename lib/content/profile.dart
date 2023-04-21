f8741135de4ba0f080319ccc3e6dda729e3cd4fc
pablogarc <juan.garo29si@gmail.com>
2023-04-21 09:58:12 -0600

    Updated events_content and explore_images

--- lib/content/profile.dart
+++ lib/content/profile.dart
@@ -25,18 +25,21 @@ class _ProfileState extends State<Profile> with TickerProviderStateMixin {
                 Container(
                   margin: EdgeInsets.only(top: 10, left: 10, right: 10),
                   child: ListTile(
-                    leading: Image.network(
-                      "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse3.mm.bing.net%2Fth%3Fid%3DOIP.8DDnZg9-q5BkyTcfOAYsbwHaGA%26pid%3DApi&f=1&ipt=7467acdf6413d32d971e931d16ff58ba57ef92753e5476e53d6842ea03b599e4&ipo=images",
+                    leading: CircleAvatar(
+                      radius: 48,
+                      backgroundImage: NetworkImage(
+                        "https://avatars.githubusercontent.com/u/52970365?v=4",
+                      ),
                     ),
                     title: Text(
-                      "name",
+                      "Pablito",
                       style: TextStyle(
                         fontFamily: 'Lobster',
                         fontSize: 24,
                       ),
                     ),
                     subtitle: Text(
-                      "tipo de cuenta",
+                      "21-04-2023",
                       style: TextStyle(
                         fontFamily: 'Lobster',
                         fontSize: 20,
